43aa49219b721d11140ca2c566ed1eb409188933
Adrian Görisch <adrian.goerisch@10m.de>
2020-07-21 14:43:22 +0200

    added Logo

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,7 +5,7 @@ const routes: Routes = [
     { path: '', redirectTo: 'home', pathMatch: 'full' },
     { path: 'home', loadChildren: () => import('./home/home.module').then(m => m.HomeModule) },
     { path: 'cofe-shop', loadChildren: () => import('./home/home.module').then(m => m.HomeModule) },
-    { path: 'to-go', loadChildren: () => import('./home/home.module').then(m => m.HomeModule) },
+    { path: 'wake-up', loadChildren: () => import('./home/home.module').then(m => m.HomeModule) },
     { path: 'coffee-break', loadChildren: () => import('./home/home.module').then(m => m.HomeModule) },
     { path: '**', loadChildren: () => import('./page-not-found/page-not-found.module').then(m => m.PageNotFoundModule) }
 ];
